8af0d61c46c051ef017cddd03626c1f753d3aa80
BugGreedy <desire.cutton.bug.1130@gmail.com>
2021-05-05 10:35:38 +0900

    update

--- ex.php
+++ ex.php
@@ -1,7 +1,13 @@
 <?php
 // echo "こんにちは、\nここから２行目です。\n";
 // echo 'こんにちは、\nここから２行目です。';
-$sum = 10+20;
-echo $sum,"\n";
-echo '$sum',"\n";
-echo "$sum","\n";
+
+// $sum = 10+20;
+// echo $sum,"\n";
+// echo '$sum',"\n";
+// echo "$sum","\n";
+
+
+$sum = 10 + 20;
+echo "\$sumの内容は、{10+20}です。";
+?>
